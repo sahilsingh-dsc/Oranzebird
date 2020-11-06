059643bce834ef92df6421e1a6a72c1b908002e8
Sahil Singh <sahilsingh.dsc@gmail.com>
2020-11-06 10:15:09 +0530

    address, favourite, contact, profile

---
+++ app/src/main/java/com/tetravalstartups/oranzebird/modules/fragment/AddressFragment.java
@@ -0,0 +1,26 @@
+package com.tetravalstartups.oranzebird.modules.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tetravalstartups.oranzebird.R;
+
+public class AddressFragment extends Fragment {
+
+    public AddressFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_address, container, false);
+    }
+}
